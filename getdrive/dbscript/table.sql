5af5e01476bfd91de61c16a16b0e6623b4444a3b
Terapico <89236406+younghwalkim@users.noreply.github.com>
2024-03-29 08:09:54 +0900

    Update table.sql

--- getdrive/dbscript/table.sql
+++ getdrive/dbscript/table.sql
@@ -5,7 +5,7 @@
 
 /*
 	ver 0.5 / 김영활 / 2024.03.29
-                : 검색테이블 칼럼 추가 및 트리거 4개 수정
+                : 검색테이블 칼럼 추가 및 트리거 4개 삭제
 
 	ver 0.4 / 홍은비 / 2024.03.27
 		: 4-1. 카테고리 테이블 CT_NAME 컬럼 추가
@@ -476,48 +476,6 @@ COMMENT ON COLUMN TB_ALARM_CHECK.AC_TID IS '팀 고유번호';
    COMMENT ON COLUMN "TB_SEARCH"."S_ID" IS '원 게시물의 고유번호';
    COMMENT ON COLUMN "TB_SEARCH"."S_CTID" IS '카테고리';
    COMMENT ON COLUMN "TB_SEARCH"."S_TID" IS '팀 고유번호';
-
-
-
--- 트리거 추가 2024.03.29 김영활 수정
-
---  게시판테이블 등록시 검색테이블에 등록
-create or replace TRIGGER TRI_INSERT_BD_Search
-AFTER INSERT ON TB_Board
-FOR EACH ROW
-BEGIN
-  INSERT INTO TB_Search ( S_NO, S_MENU, S_TITLE, S_CONTENT, S_DATE, S_CRUID, S_ID, S_CTID, S_TID )
-  VALUES ( (select count(s_no)+1 from tb_search) , 'BD', :NEW.B_Title,  :NEW.B_CONTENT,  :NEW.B_CDATE,  :NEW.B_CRUID,  :NEW.B_NO,  :NEW.B_CTID, :NEW.B_TID);
-END;
-
---  게시판테이블 수정시 검색테이블에 등록
-create or replace TRIGGER TRI_update_BD_Search
-AFTER update ON TB_Board
-FOR EACH ROW
-BEGIN
-  INSERT INTO TB_Search ( S_NO, S_MENU, S_TITLE, S_CONTENT, S_DATE, S_CRUID, S_ID, S_CTID, S_TID )
-  VALUES ( (select max(s_no)+1 from tb_search), 'BD', :NEW.B_Title,  :NEW.B_CONTENT,  :NEW.B_UDATE,  :NEW.B_CRUID,  :NEW.B_NO,  :NEW.B_CTID, :NEW.B_TID);
-END;
-
---   드라이브테이블 등록시 검색테이블에 등록
-create or replace TRIGGER TRI_INSERT_DV_Search
-AFTER INSERT ON TB_Drive_file
-FOR EACH ROW
-BEGIN
-  INSERT INTO TB_Search ( S_NO, S_MENU, S_TITLE, S_CONTENT, S_DATE, S_CRUID, S_ID, S_CTID, S_TID )
-  VALUES ( (select max(s_no)+1 from tb_search) , 'DV', :NEW.FL_NAME,  :NEW.FL_NAME,  :NEW.FL_CDATE,  :NEW.FL_CRUID,  :NEW.FL_NO, :NEW.FL_DID,  :NEW.FL_TID);
-END;
-
---   드라이브테이블 수정시 검색테이블에 등록
-create or replace TRIGGER TRI_update_DV_Search
-AFTER INSERT ON TB_Drive_file
-FOR EACH ROW
-BEGIN
-  INSERT INTO TB_Search ( S_NO, S_MENU, S_TITLE, S_CONTENT, S_DATE, S_CRUID, S_ID, S_CTID, S_TID )
-  VALUES ( (select max(s_no)+1 from tb_search) , 'DV', :NEW.FL_NAME,  :NEW.FL_NAME,  :NEW.FL_UDATE,  :NEW.FL_CRUID,  :NEW.FL_NO, :NEW.FL_DID,  :NEW.FL_TID);
-END;
-
-
 
 
 -- 10. INSERT문 (샘플 데이터)
